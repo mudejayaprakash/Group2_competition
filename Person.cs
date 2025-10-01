5aa11ae2b5ffecd7c2c5587b0566dfd2f75255cd
Pradeep-usf <pradeepanand@usf.edu>
2025-09-30 21:41:52 -0400

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USF_Library_Competition_Week6
+{
+    public class Person
+    {
+        public string Name { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Id { get; set; } = "";
+
+        public Person() { }
+
+        public Person(string name, string email, string id)
+        {
+            Name = name;
+            Email = email;
+            Id = id;
+        }
+
+        public override string ToString() => $"Name: {Name}, ID: {Id}";
+    }
+}
