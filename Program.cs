a21c164447fb87eeb6700cac21ffc3484fc438f9
mudejayaprakash <mudejayaprakash@github.com>
2025-09-30 22:31:45 -0400

    Updated Program.cs with Staff example

--- Program.cs
+++ Program.cs
@@ -9,6 +9,12 @@ namespace USF_Library_Competition_Week6
             var john = new Person("John Snow", "johnsnow@gmail.com", "LIB12345");
             Console.WriteLine(john);                   // "Name: John Snow, ID: LIB12345"
             Console.WriteLine($"Email: {john.Email}"); // "johnsnow@gmail.com"
+            
+            var staff1 = new Staff("Grandon Gill", "grandon@usf.edu", "ST001", "Librarian", "Library Services");
+            Console.WriteLine("---- Staff Example ----");
+            Console.WriteLine(staff1);
+            Console.WriteLine($"Email: {staff1.Email}");
         }
+        
     }
 }
